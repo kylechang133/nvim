f3d3429b8ce857c21270d3e534a701c199ba7798
Kyle Chang <59430996+kylechang133@users.noreply.github.com>
2024-03-23 17:27:30 -0600

    Delete lua directory

--- lua/vim_options.lua
+++
@@ -1,9 +0,0 @@
--- Tab Settings
-vim.cmd("set expandtab")
-vim.cmd("set tabstop=4")
-vim.cmd("set softtabstop=4")
-vim.cmd("set shiftwidth=4")
-vim.cmd("set autoindent")
-
--- Leader Keybind Setup
-vim.g.mapleader = " "
