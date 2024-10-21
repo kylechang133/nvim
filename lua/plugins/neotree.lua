105674e75b5c56e256fa9f7624cd160e7b0a80d8
Kyle Chang <59430996+kylechang133@users.noreply.github.com>
2024-10-21 14:24:44 -0600

    Added oil.nvim plugin

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -1,14 +1,28 @@
--- Neotree (file explorer)
 return {
-	"nvim-neo-tree/neo-tree.nvim",
-	branch = "v3.x",
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-		"nvim-tree/nvim-web-devicons",
-		"MunifTanjim/nui.nvim",
+
+	{
+		-- Neotree (file explorer)
+		"nvim-neo-tree/neo-tree.nvim",
+		branch = "v3.x",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-tree/nvim-web-devicons",
+			"MunifTanjim/nui.nvim",
+		},
+
+		config = function()
+			vim.keymap.set("n", "<C-n>", ":Neotree toggle filesystem left<CR>", {})
+		end,
 	},
 
-	config = function()
-		vim.keymap.set("n", "<C-n>", ":Neotree toggle filesystem left<CR>", {})
-	end,
+	{
+		-- oil.nvim (file explorer)
+		"stevearc/oil.nvim",
+		config = function()
+			require("oil").setup({
+				default_file_explorer = true,
+			})
+			vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+		end,
+	},
 }
