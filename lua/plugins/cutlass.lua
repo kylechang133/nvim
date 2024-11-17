b77c91c02fba29666ff2ac6888a6f49c0bc11073
Kyle Chang <59430996+kylechang133@users.noreply.github.com>
2024-11-16 21:06:39 -0700

    created a cut_key for cutlass.nvim plugin

--- lua/plugins/cutlass.lua
+++ lua/plugins/cutlass.lua
@@ -1,5 +1,14 @@
 -- Override default vim cut and paste behaviour
 return {
-    "gbprod/cutlass.nvim",
-    opts = {},
+	"gbprod/cutlass.nvim",
+	opts = {
+		cut_key = "m",
+	},
+
+	config = function()
+		vim.keymap.set("n", "m", "d", { silent = true })
+		vim.keymap.set("x", "m", "d", { silent = true })
+		vim.keymap.set("n", "mm", "dd", { silent = true })
+		vim.keymap.set("n", "M", "D", { silent = true })
+	end,
 }
