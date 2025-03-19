fb59e92786b65c08fd8a96397ea35b17d15b7b9c
Kyle Chang <59430996+kylechang133@users.noreply.github.com>
2025-03-18 18:22:52 -0600

    add more keymappings

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -25,5 +25,11 @@ vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "<C-f>", "<C-f>zz")
 vim.keymap.set("n", "<C-b>", "<C-b>zz")
 
+
+-- Better Indent
+vim.keymap.set("v", "<", "<gv")
+vim.keymap.set("v", ">", ">gv")
+
+
 -- Toggle Case
-vim.keymap.set("n", "<leader>t", "bv~")
+vim.keymap.set("v", "<leader>t", "~")
