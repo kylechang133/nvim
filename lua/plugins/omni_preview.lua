e3d84cb1a9bde5e018dcbf7f6b64850188357981
Kyle Chang <59430996+kylechang133@users.noreply.github.com>
2025-12-06 18:58:40 -0700

    Add omni preview plugin

---
+++ lua/plugins/omni_preview.lua
@@ -0,0 +1,9 @@
+-- Preview files
+return {
+	"sylvanfranklin/omni-preview.nvim",
+	opts = {},
+	keys = {
+		{ "<leader>po", "<cmd>OmniPreview start<CR>", desc = "OmniPreview Start" },
+		{ "<leader>pc", "<cmd>OmniPreview stop<CR>", desc = "OmniPreview Stop" },
+	},
+}
