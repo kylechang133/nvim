5ea956d307f4bd17f17c59a2d85f975c12978e5a
Kyle Chang <59430996+kylechang133@users.noreply.github.com>
2025-12-15 16:35:47 -0700

    Remove comment

--- lua/plugins/fzf_lua.lua
+++ lua/plugins/fzf_lua.lua
@@ -5,7 +5,7 @@ return {
         local fzf = require("fzf-lua")
 
         fzf.setup(opts)
-        fzf.register_ui_select() -- replace telescope-ui-select
+        fzf.register_ui_select()
 
         vim.keymap.set("n", "<leader>ff", fzf.files, { desc = "Find files" })
         vim.keymap.set("n", "<leader>fw", fzf.live_grep, { desc = "Live grep" })
